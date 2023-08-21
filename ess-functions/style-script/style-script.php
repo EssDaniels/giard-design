ccd70c1058f8ed97202afd81327e64bda892889e
Dawid Skibński <skibol2@wp.pl>
2023-08-21 12:27:54 +0200

    Add dependencies

--- ess-functions/style-script/style-script.php
+++ ess-functions/style-script/style-script.php
@@ -2,6 +2,6 @@
 function wpdocs_theme_name_scripts()
 {
     wp_enqueue_style('ess', get_template_directory_uri() . '/dist/css/main.css', array(), '1.0.0');
-    wp_enqueue_script('ess', get_template_directory_uri() . '/dist/js/main.js', array(), '1.0.0', true);
+    wp_enqueue_script('ess', get_template_directory_uri() . '/dist/js/main.js', array('jquery'), '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'wpdocs_theme_name_scripts');
