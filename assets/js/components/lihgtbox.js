6289714658a0c294f6b649a461177a10f4c59c59
Dawid Skibński <skibol2@wp.pl>
2023-08-23 15:45:52 +0200

    set lightbox

---
+++ assets/js/components/lihgtbox.js
@@ -0,0 +1,18 @@
+import lightbox from './../../../node_modules/lightbox2/dist/js/lightbox.min.js';
+
+
+
+export default function LightboxComponent() {
+  (function ($) {
+
+
+    lightbox.option({
+      'resizeDuration': 200,
+      'wrapAround': true
+    })
+    
+
+
+    
+})(jQuery);
+}
