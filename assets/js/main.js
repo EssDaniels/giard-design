57d590db8e174a6adb90a50c45a0c07cc52b9604
Dawid Skibński <skibol2@wp.pl>
2023-08-23 15:51:00 +0200

    set main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,45 @@
 
+import $ from 'jquery';
+import dropdown from './components/dropdown';
+import masonryComponent from './components/masonry';
+import loadImagesAjax from './components/load-images-ajax';
+import scrollTo from './components/scroll-to';
+import LightboxComponent from './components/lihgtbox';
+import slickComponent from './components/slick-component';
+import { macyInstance } from './components/masonry';
+import headerSticky from './components/header-sticky';
+
+
+(function ($) {
+
+  // jQuery test
+  if (window.jQuery) {
+      console.log('jQuery is working...');
+  }
+
+})(jQuery);
+
+
+var documentReady = function () {
+ 
+  dropdown();
+  scrollTo();
+  LightboxComponent();
+  slickComponent();
+  masonryComponent();
+  loadImagesAjax();
+  headerSticky();
+}
+
+
+if (
+  document.readyState === "complete" ||
+  (document.readyState !== "loading" && !document.documentElement.doScroll)
+) {
+  documentReady();
+} else {
+  document.addEventListener("DOMContentLoaded", documentReady);
+}
+
+
+export { macyInstance };
