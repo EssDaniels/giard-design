cb8758ec256f92f590f5190164fb1852d3f98067
Dawid Skibński <skibol2@wp.pl>
2023-09-26 17:58:19 +0200

    dodanie sprawdzenie czy istnieje galeria

--- assets/js/components/masonry.js
+++ assets/js/components/masonry.js
@@ -3,21 +3,23 @@ import Macy from 'macy';
 export let macyInstance;
 
 export default function masonryComponent() {
-
-    macyInstance = Macy({
-        container: '.masonry-gallery',
-        trueOrder: false,
-        waitForImages: true,
-        margin: 24,
-        columns: 3,
-        breakAt: {
-            1200: 3,
-            940: 2,
-            520: 1
-        }
-      });
-    
-      macyInstance.recalculate();
-     
+    // Sprawdzenie, czy element istnieje w DOM
+    if (document.querySelector('.masonry-gallery')) {
+        macyInstance = Macy({
+            container: '.masonry-gallery',
+            trueOrder: false,
+            waitForImages: true,
+            margin: 24,
+            columns: 3,
+            breakAt: {
+                1200: 3,
+                940: 2,
+                520: 1
+            }
+        });
+        
+        macyInstance.recalculate();
+    }
 }
+
 
